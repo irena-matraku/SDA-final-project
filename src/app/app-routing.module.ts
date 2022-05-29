142d77dc0b6ae2a80f3fc5acaf49499aca417208
Irena Matraku <irenamatraku@gmail.com>
2022-05-30 00:00:50 +0200

    changes in app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { CreateNewProjectComponent } from './create-new-project/create-new-project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent},
+  { path: 'navbar', component: NavbarComponent},
+  { path: 'newproject', component: CreateNewProjectComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomepageComponent } from './homepage/homepage.component';
+import { CreateNewProjectComponent } from './create-new-project/create-new-project.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    CreateNewProjectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
