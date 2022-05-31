826941815304e4d4274330e7614f1374b154a778
paula <paulaxhindoli@gmail.com>
2022-05-31 23:09:39 +0200

    complete form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -33,6 +37,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatRadioModule,
     MatNativeDateModule,
     MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
