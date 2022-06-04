333a6a8de9e349cea74975831df03b938f2dfb4b
Irena Matraku <64260395+irena-matraku@users.noreply.github.com>
2022-06-04 12:46:21 +0200

    renamed folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomepageComponent } from './homepage/homepage.component';
-import { CreateNewProjectComponent } from './create-new-project/create-new-project.component';
+import { CreateNewProjectComponent } from './projects/create-new-project/create-new-project.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
-import { CreatedProjectComponent } from './created-project/created-project.component';
+import {CreatedProjectComponent } from './projects/created-project/created-project.component';
 import { FooterComponent } from './core/footer/footer.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {CdkAccordionModule} from '@angular/cdk/accordion';
---
+++ src/app/shared/mock-projects.ts
@@ -0,0 +1,15 @@
+// import { Project } from "./projects";
+
+
+
+// export const PROJECTS: Project[] = [
+//   {
+//     projectName: 'SDA Final Project',
+//     projectDescription: 'Lorem lorem',
+//     projectTeamMembers: ['Irena Matraku', 'Paula Xhindoli'],
+//     projectCategories: ['Design', 'Development', 'DevOps'],
+//     projectStatus: ['To be done', 'In progress', 'Done']
+//   }
+  
+
+// ]
