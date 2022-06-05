ab5affd59c417627284f26f50e1e59318fe24037
Irena Matraku <64260395+irena-matraku@users.noreply.github.com>
2022-06-05 12:19:33 +0200

    feat(structural changes in app) last changes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { ProjectUpsertComponent } from './projects/project-upsert/project-upsert.component';
-import { ProjectIndexComponent } from './projects/project-index/project-index.component';
-import { TaskCreateComponent as TaskCreateComponent } from './tasks/tasks-create/tasks-create.component';
+import { ProjectUpsertComponent } from './pages/projects/project-upsert/project-upsert.component';
+import { ProjectIndexComponent } from './pages/projects/project-index/project-index.component';
+import { TaskCreateComponent as TaskCreateComponent } from './pages/tasks/tasks-create/tasks-create.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/layout/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { ProjectUpsertComponent } from './projects/project-upsert/project-upsert.component';
+import { ProjectUpsertComponent } from './pages/projects/project-upsert/project-upsert.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TaskCreateComponent } from './tasks/tasks-create/tasks-create.component';
+import { TaskCreateComponent } from './pages/tasks/tasks-create/tasks-create.component';
 
 
 
@@ -17,25 +17,24 @@ import { TaskCreateComponent } from './tasks/tasks-create/tasks-create.component
 import {MatChipsModule} from '@angular/material/chips';
 import {MatFormFieldModule, } from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
-import { ProjectIndexComponent } from './projects/project-index/project-index.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
-import {MatSelectModule} from '@angular/material/select';
+import { ProjectIndexComponent } from './pages/projects/project-index/project-index.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule} from '@angular/forms';
-import { TaskEditComponent } from './tasks/tasks-edit/tasks-edit.component';
-import { TasksIndexComponent } from './tasks/tasks-index/tasks-index.component';
+import { TaskEditComponent } from './pages/tasks/tasks-edit/tasks-edit.component';
+import { TasksIndexComponent } from './pages/tasks/tasks-index/tasks-index.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { ProjectCardComponent } from './shared/cards/project-card/project-card.component';
 import { TaskCardComponent } from './shared/cards/task-card/task-card.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
-
 
 
 let projects = localStorage.getItem('projects');
@@ -70,7 +69,6 @@ if(projects == null) {
     TaskCardComponent,
     LoginComponent,
     RegisterComponent,
-    
     
   ],
   imports: [
